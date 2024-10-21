db0e37b2a6ef7a85ea2c2cb524618cd729bc7af2
imad273 <abbadimad0123@gmail.com>
2024-10-21 09:22:14 +0100

    add active link to sidebar

--- admin-panel/src/components/Sidebar.tsx
+++ admin-panel/src/components/Sidebar.tsx
@@ -6,8 +6,11 @@ import {
 } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
+import { useState } from 'react';
 
 const Sidebar = () => {
+  const [activeLink, setActiveLink] = useState("dashboard");
+
   return (
     <div className='p-2'>
       <div className='flex items-center justify-center w-full my-8'>
@@ -16,19 +19,19 @@ const Sidebar = () => {
       </div>
 
       <div className='mx-2 mt-12 space-y-2'>
-        <Link to="/" className='w-full px-3 py-3.5 rounded-lg flex items-center gap-2 text-gray-300 hover:bg-dark_content_bg hover:text-white duration-200'>
+        <Link onClick={() => setActiveLink("dashboard")} to="/" className={`${activeLink === "dashboard" && "bg-dark_content_bg"} w-full px-3 py-3.5 rounded-lg flex items-center gap-2 text-gray-300 hover:bg-dark_content_bg hover:text-white duration-200`}>
           <LayoutDashboard size={20} />
           Dashboard
         </Link>
-        <Link to="/rooms" className='w-full px-3 py-3.5 rounded-lg flex items-center gap-2 text-gray-300 hover:bg-dark_content_bg hover:text-white duration-200'>
+        <Link onClick={() => setActiveLink("rooms")} to="/rooms" className={`${activeLink === "rooms" && "bg-dark_content_bg"} w-full px-3 py-3.5 rounded-lg flex items-center gap-2 text-gray-300 hover:bg-dark_content_bg hover:text-white duration-200`}>
           <School size={20} />
           Rooms
         </Link>
-        <Link to="/reservations" className='w-full px-3 py-3.5 rounded-lg flex items-center gap-2 text-gray-300 hover:bg-dark_content_bg hover:text-white duration-200'>
+        <Link onClick={() => setActiveLink("reservations")} to="/reservations" className={`${activeLink === "reservations" && "bg-dark_content_bg"} w-full px-3 py-3.5 rounded-lg flex items-center gap-2 text-gray-300 hover:bg-dark_content_bg hover:text-white duration-200`}>
           <Album size={20} />
           Reservations
         </Link>
-        <Link to="/staff" className='w-full px-3 py-3.5 rounded-lg flex items-center gap-2 text-gray-300 hover:bg-dark_content_bg hover:text-white duration-200'>
+        <Link onClick={() => setActiveLink("staff")} to="/staff" className={`${activeLink === "staff" && "bg-dark_content_bg"} w-full px-3 py-3.5 rounded-lg flex items-center gap-2 text-gray-300 hover:bg-dark_content_bg hover:text-white duration-200`}>
           <UsersRound size={20} />
           Staff
         </Link>
